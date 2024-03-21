e7a0bdb331d1ed97220b8d1ead3ae15271879e6e
Margarita Varitskaya <m.varitskaya@netology-group.ru>
2024-03-21 22:01:16 +0300

    fix 1

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -2,38 +2,3 @@
 import playGameCalc from '../src/games/game-calc.js';
 
 playGameCalc();
-
-//import readlineSync from 'readline-sync';
-//import startG from './brain-games.js';
-//import { name } from '../src/cli.js';
-//import {    cons, car, cdr, toString, isPair,  } from '@hexlet/pairs';
-
-//startG();
-
-//console.log('What is the result of the expression?');
-
-//function startGame () {
-    
-   // for (let n = 0; n < 3; n++) {
-   //     const pair = cons(a, b);
-    //    var array = ['+', '-', '*'];
-    //    var randomPunctationMark= array[Math.floor(Math.random() * array.length)];
-
-
-    //    console.log(`Question: ${a}${randomPunctationMark}${b}`);
-    //    const userAnswer = readlineSync.question('Your answer: ');
-    //        if (userAnswer === fuuu && n < 2) {
-    //        console.log(`Correct!`);
-   //         }
-    //        else if (userAnswer === fuuu && n === 2) {
-      //        console.log(`Congratulations, ${name}`);
-      //        return;
-      //      }
-      //      else {
-     //           console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${fuuu}'.`);
-   //             return;
-    //      }
-///    }
-//};
-
-//startGame();
