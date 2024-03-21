17c5d37a8f20fa4d7cac31948f21c60ae1e841bb
Margarita Varitskaya <m.varitskaya@netology-group.ru>
2024-03-21 23:11:19 +0300

    fix brain games

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,7 @@ import readlineSync from 'readline-sync';
 
 let name;
 function showGreeting() {
+  console.log('Welcome to the Brain Games!');
   console.log('May I have your name?');
   name = readlineSync.question('Your answer: ');
   console.log(`Hello, ${name}!`);
