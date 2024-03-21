8a71d425ba9bd05836ce7b16838a777388d293ed
Margarita Varitskaya <m.varitskaya@netology-group.ru>
2024-03-21 22:55:37 +0300

    fix

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -4,7 +4,7 @@ import playGame from '../index.js';
 const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const getCorrectAnswer = (num) => {
-  for (let i = 2; i < num; i++) {
+  for (let i = 2; i < num; i += 1) {
     if (num % i === 0) return false;
   }
   return true;
